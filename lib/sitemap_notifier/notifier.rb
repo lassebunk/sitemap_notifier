570ed87f2a5889b5b62e72fb62ea8a89d88bec95
Lasse Bunk <lassebunk@gmail.com>
2011-09-20 20:11:54 +0200

    using background process

--- lib/sitemap_notifier/notifier.rb
+++ lib/sitemap_notifier/notifier.rb
@@ -8,25 +8,29 @@ module SitemapNotifier
       attr_accessor :delay
       attr_accessor :sitemap_url
       attr_accessor :environments
+      attr_accessor :running_pid
       
       def notify!
         raise "environments not set – use SitemapNotifier::Notifier.environments = [:xx, :xx]" unless environments
         raise "delay not set – use SitemapNotifier::Notifier.delay = xx" unless delay
         raise "sitemap_url not set – use SitemapNotifier::Notifier.sitemap_url = 'xx'" unless sitemap_url
         
-        if environments.include?(Rails.env.to_sym)
-          if @thread.nil? || @thread.status == false
-            @thread = Thread.new do
-              Rails.logger.info "Notifying search engines of changes to sitemap..."
+        if environments.include?(Rails.env.to_sym) && !running?
+          self.running_pid = fork do
+            Rails.logger.info "Notifying search engines of changes to sitemap..."
 
-              get_url "http://www.google.com/webmasters/sitemaps/ping?sitemap=#{CGI::escape(sitemap_url)}"
-              get_url "http://www.bing.com/webmaster/ping.aspx?siteMap=#{CGI::escape(sitemap_url)}"
-              get_url "http://submissions.ask.com/ping?sitemap=#{CGI::escape(sitemap_url)}"
-              # no Yahoo here, as they will be using Bing from september 15th, 2011
+            get_url "http://www.google.com/webmasters/sitemaps/ping?sitemap=#{CGI::escape(sitemap_url)}"
+            get_url "http://www.bing.com/webmaster/ping.aspx?siteMap=#{CGI::escape(sitemap_url)}"
+            get_url "http://submissions.ask.com/ping?sitemap=#{CGI::escape(sitemap_url)}"
+            # get_url "http://localhost:3000/ping?sitemap=#{CGI::escape(sitemap_url)}"
+            # no Yahoo here, as they will be using Bing from september 15th, 2011
 
-              sleep delay
-            end
+            sleep delay
+
+            exit
           end
+          
+          puts running_pid
         end
       end
       
@@ -34,7 +38,21 @@ module SitemapNotifier
     
       def get_url(url)
         uri = URI.parse(url)
-        Net::HTTP.get_response(uri)
+        begin
+          Net::HTTP.get_response(uri)
+        rescue Exception
+        end
+      end
+      
+      def running?
+        return false unless running_pid
+        
+        begin
+          Process.getpgid(running_pid)
+          true
+        rescue Errno::ESRCH
+          false
+        end
       end
     end
   end
