ea81e80bda00a4ee87e747f1b252fa69e23ef44b
Lasse Bunk <lassebunk@gmail.com>
2013-02-05 20:43:29 +0100

    Use Fakeweb for testing requests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,13 @@ require 'test/unit'
 require 'sitemap_notifier'
 require 'db'
 require 'timecop'
+require 'fakeweb'
+
+# Catch requests to Google and Bing
+[%r{http://www\.google\.com/webmasters/sitemaps/ping\?sitemap=},
+ %r{http://www\.bing\.com/webmaster/ping\.aspx\?siteMap=}].each do |url|
+   FakeWeb.register_uri(:get, url, :body => "OK!")  
+end
+
+# Catch all other HTTP requests and make them fail so we don't accidentally make real HTTP requests in the test suite
+FakeWeb.register_uri(:any, //, :body => "Fail! (change this in test_helper.rb)", :status => ["400", "Bad Request"])
